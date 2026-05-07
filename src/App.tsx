1e56c186bbdc88f037db4753523b117844b205b0
UGTSanidadSalamanca <fespugtsalamanca@gmail.com>
2026-05-07 11:51:59 +0200

    Update link to Google Drive file in App.tsx def triptico def

--- src/App.tsx
+++ src/App.tsx
@@ -186,7 +186,7 @@ export default function App() {
               <span>Guía Oficial UGT (Web)</span>
             </a>
             <a 
-              href="https://drive.google.com/file/d/1In3ujTr-QpvjJJkJed7PFqDyJJgnKjY5/view?usp=link"
+              href="https://drive.google.com/file/d/1-HlODcSxdBMNgTLu-fO22BDlJvbObzzJ/view?usp=link"
               target="_blank"
               rel="noopener noreferrer"
               onClick={() => setIsMobileMenuOpen(false)}
