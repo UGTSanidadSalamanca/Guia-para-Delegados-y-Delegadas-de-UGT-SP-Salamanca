b02fdf191d6af672beff21ec5051909cbd3481b3
UGTSanidadSalamanca <fespugtsalamanca@gmail.com>
2026-05-07 21:23:08 +0200

    Update link to official UGT guide 7_5_26

--- src/App.tsx
+++ src/App.tsx
@@ -186,7 +186,7 @@ export default function App() {
               <span>Guía Oficial UGT (Web)</span>
             </a>
             <a 
-              href="https://drive.google.com/file/d/1-HlODcSxdBMNgTLu-fO22BDlJvbObzzJ/view?usp=link"
+              href="https://drive.google.com/file/d/11W-dBjSs-gN8BXeHxiYc60UfK-nP1tFK/view?usp=link"
               target="_blank"
               rel="noopener noreferrer"
               onClick={() => setIsMobileMenuOpen(false)}
